e418c607b7236c9635f4643e8c00f15b5b29d8f3
Axel Ramiro <axel@alchimia.mx>
2021-08-02 17:43:47 -0500

    Prueba inicial

--- src/main/java/com/example/MySpringBootRouter.java
+++ src/main/java/com/example/MySpringBootRouter.java
@@ -17,54 +17,50 @@ public class MySpringBootRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
-        // restConfiguration()
-        // .component("servlet")
-        // .bindingMode(RestBindingMode.auto);
+        restConfiguration()
+        	.component("servlet")
+        	.bindingMode(RestBindingMode.auto);
 
-        // rest("/api")
-        // // Servicio Post
-        // .post()
-        // .type(BeanDatos.class)
-        // .route()
-        // .log("The body is ${body.name}!!")
-        // .transform().simple("The name in body is ${body.saludo()}")
-        // .endRest()
-        // // Servicio Post2
-        // .post("/datos")
-        // .type(BeanDatos.class)
-        // .route()
-        // .log("The body is ${body.name}!!")
-        // .process(new Processor() {
-        //     @Override
-        //     public void process(Exchange exchange) throws Exception {
-        //         BeanDatos beanDatos = exchange.getMessage().getBody(BeanDatos.class);
-        //         beanDatos.setEdad("15");
-        //         exchange.getMessage().setBody(beanDatos);
-        //     }
-        // })
-        // .endRest()
-        // // Servicio Get
-        // .get()
-        // .route()
-        // .log("The body is ${body}")
-        // .transform().simple("The body is ${body}")
-        // .endRest();
+        rest("/api")
+        	.post("/customer")
+        	//.type()
+        	.route()
+        	.log("The body is ${body}!!")
+        	.transform().simple("The name in body is ${body}")
+        	.endRest()
+        	
+        	.put("/customer")
+        	//.type()
+        	.route()
+        	.log("The body is ${body}!!")
+//        	.process(new Processor() {
+//	            @Override
+//	            public void process(Exchange exchange) throws Exception {
+//	                BeanDatos beanDatos = exchange.getMessage().getBody(BeanDatos.class);
+//	                beanDatos.setEdad("15");
+//	                exchange.getMessage().setBody(beanDatos);
+//	            }
+//        	})
+        	.to("https://run.mocky.io/v3/3334df41-5550-4527-8a2a-fb682dae4f8f?bridgeEndpoint=true&throwExceptionOnFailure=false")
+        	.to("stream:out")
+        	//.log("${body}")
+        	.endRest();
 
-        from("timer:hello?period={{timer.period}}").routeId("hello")
-            .transform().method("myBean", "saySomething")
-            .filter(simple("${body} contains 'foo'"))
-                .to("log:foo")
-            .end()
-            .to("stream:out")
-            .to("https://run.mocky.io/v3/73666d43-8da3-4b02-b648-8121e80c4232")
-//            .to("https://run.mocky.io/v3/4e1f861e-6a7f-4ca1-85e7-e35bceee5b51")
-//            .to("stream:header")
-	        .choice()
-	        .when(simple("${header.CamelHttpResponseCode} == '200'"))
-	        	.to("stream:out")
-	        .otherwise()
-	            .log("No tenia datos la respuesta");
-//        http://10.1.99.27:8080/integrator/receipt-simple-confirm?between={"start": "2018-05-01", "end": "2018-06-12"}
+//        from("timer:hello?period={{timer.period}}").routeId("hello")
+//            .transform().method("myBean", "saySomething")
+//            .filter(simple("${body} contains 'foo'"))
+//                .to("log:foo")
+//            .end()
+//            .to("stream:out")
+            
+         //  .to("https://run.mocky.io/v3/4e1f861e-6a7f-4ca1-85e7-e35bceee5b51")
+           //.to("stream:header")
+//	        .choice()
+//	        .when(simple("${header.CamelHttpResponseCode} == '200'"))
+//	        	.to("stream:out")
+//	        .otherwise()
+//	            .log("No tenia datos la respuesta");
+       // http://10.1.99.27:8080/integrator/receipt-simple-confirm?between={"start": "2018-05-01", "end": "2018-06-12"}
     }
 
 }
